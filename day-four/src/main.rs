1acf79a3a5f09795ff579f8ba2bb15d19751b401
nitame <>
2022-12-04 12:06:16 +0100

    day four puzzle 2

--- day-four/src/main.rs
+++ day-four/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Advent of code day 4");
 
     let file_path = get_file_path(String::from("input.txt"));
-    let result = puzzle(file_path);
+    let result = puzzle_2(file_path);
 
     println!("result -> {result}");
 }
@@ -44,6 +44,25 @@ fn puzzle(file_path: PathBuf) -> i32 {
     total
 }
 
+fn puzzle_2(file_path: PathBuf) -> i32 {
+    let mut total = 0;
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(line) = line {
+                // logic puzzle
+                let split_elves_section: Vec<_> = line.split(",").collect();
+                let left_elf_section_bounds: Vec<_> = split_elves_section[0].split("-").collect();
+                let right_elf_section_bounds: Vec<_> = split_elves_section[1].split("-").collect();
+
+                if section_overlap(&left_elf_section_bounds, &right_elf_section_bounds) {
+                    total = total + 1
+                }
+            }
+        }
+    }
+    total
+}
+
 fn get_file_path(filename: String) -> PathBuf {
     let current_dir = env::current_dir().expect("Attempt to get current dir");
     current_dir.join(Path::new(filename.as_str()))
@@ -58,15 +77,28 @@ fn section_contains(left_section: &Vec<&str>, right_section: &Vec<&str>) -> bool
     left_start_bound <= right_start_bound && left_end_bound >= right_end_bound
 }
 
+fn section_overlap(left_section: &Vec<&str>, right_section: &Vec<&str>) -> bool {
+    let left_start_bound: i32 = left_section[0].parse().unwrap();
+    let left_end_bound: i32 = left_section[1].parse().unwrap();
+    let right_start_bound: i32 = right_section[0].parse().unwrap();
+    let right_end_bound: i32 = right_section[1].parse().unwrap();
+
+    let left_range = left_start_bound..=left_end_bound;
+    let right_range = right_start_bound..=right_end_bound;
+
+    (left_range.contains(&right_start_bound) || left_range.contains(&right_end_bound))
+        || (right_range.contains(&left_start_bound) || right_range.contains(&left_end_bound))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{get_file_path, puzzle};
+    use crate::{get_file_path, puzzle, puzzle_2};
 
     #[test]
     fn it_works() {
         let file_path = get_file_path(String::from("input.test.txt"));
         let result = puzzle(file_path);
-        assert_eq!(result, 3);
+        assert_eq!(result, 4);
     }
 
     #[test]
@@ -74,5 +106,19 @@ mod tests {
         let file_path = get_file_path(String::from("input.txt"));
         let result = puzzle(file_path);
         assert_eq!(result, 490);
+    }
+
+    #[test]
+    fn it_works_with_puzzle_2() {
+        let file_path = get_file_path(String::from("input.test.txt"));
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn it_works_with_whole_input_puzzle_2() {
+        let file_path = get_file_path(String::from("input.txt"));
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 921);
     }
 }
