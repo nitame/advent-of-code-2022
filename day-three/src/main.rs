7dcea69c4a17cac8b6806824c41911caddefbb1a
nitame <>
2022-12-25 15:24:47 +0100

    Day 3 puzzle 2

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -6,18 +6,17 @@ use std::{env, io};
 fn main() {
     println!("Advent of code day 3");
 
-    let total = puzzle();
-    println!("{total}");
+    let total_1 = puzzle();
+    let total_2 = puzzle_2();
+    println!("Puzzle 1 solution {total_1}");
+    println!("Puzzle 2 solution {}", total_2);
 }
 
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 fn puzzle() -> i32 {
     let current_dir = env::current_dir().expect("Attempt to get current dir");
-    println!("current dir {:?}", current_dir);
-
     let file_path = current_dir.join(Path::new("input.txt"));
-    println!("file path: {:?}", file_path);
 
     let mut total = 0;
     if let Ok(lines) = read_lines(file_path) {
@@ -44,6 +43,65 @@ fn get_priority(input: String) -> i32 {
     priority_index as i32 + 1
 }
 
+fn puzzle_2() -> u32 {
+    let current_dir = env::current_dir().expect("Attempt to get current dir");
+    let file_path = current_dir.join(Path::new("input.txt"));
+
+    let mut rucksacks_grouped_by_3_elves: Vec<Vec<String>> = Vec::new();
+    if let Ok(lines) = read_lines(file_path) {
+        let mut triple: Vec<String> = Vec::new();
+        let mut index: usize = 0;
+        for line in lines {
+            if let Ok(line) = line {
+                if index == 2 {
+                    triple.push(line.clone());
+                    rucksacks_grouped_by_3_elves.push(triple.clone());
+                    triple.pop();
+                    triple.pop();
+                    triple.pop();
+                    index = 0;
+                } else {
+                    triple.push(line);
+                    index = index + 1
+                }
+            }
+        }
+    }
+
+    let common_item_list = find_common_item_for_each_groups(rucksacks_grouped_by_3_elves);
+    let priorities = convert_to_priorities(common_item_list);
+    priorities.iter().sum()
+}
+
+fn find_common_item_for_each_groups(input: Vec<Vec<String>>) -> Vec<char> {
+    let mut common_items_list: Vec<char> = Vec::new();
+    for group in input {
+        let elf_1 = group.get(0).expect("Cannot get elf 1 items");
+        let elf_2 = group.get(1).expect("Cannot get elf 2 items");
+        let elf_3 = group.get(2).expect("Cannot get elf 3 items");
+        let common = elf_1
+            .chars()
+            .find(|c| {
+                elf_2.contains(|cc| cc == c.to_owned()) && elf_3.contains(|ccc| ccc == c.to_owned())
+            })
+            .expect("No common item found between elf 1, elf 2, elf 3");
+        common_items_list.push(common);
+    }
+    common_items_list
+}
+
+fn convert_to_priorities(input: Vec<char>) -> Vec<u32> {
+    input
+        .iter()
+        .map(|c| {
+            ALPHABET
+                .find(*c)
+                .expect("Failed to retrieve priority index")
+        })
+        .map(|value| value as u32 + 1)
+        .collect()
+}
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -54,11 +112,17 @@ where
 
 #[cfg(test)]
 mod tests {
-    use crate::puzzle;
+    use crate::{puzzle, puzzle_2};
 
     #[test]
-    fn it_works() {
+    fn it_works_puzzle_1() {
         let result = puzzle();
         assert_eq!(result, 8109);
+    }
+
+    #[test]
+    fn it_works_puzzle_2() {
+        let result = puzzle_2();
+        assert_eq!(result, 2738);
     }
 }
