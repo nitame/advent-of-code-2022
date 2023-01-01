0fb21c1b0f08d91d4b4e2f1303a95f36ea3fce40
nitame <>
2023-01-01 10:56:23 +0100

    day 5 - puzzle 2

--- day-five/src/main.rs
+++ day-five/src/main.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::BufRead;
+use std::ops::Add;
 use std::path::{Path, PathBuf};
 use std::{env, io};
 
 fn main() {
-    println!("Advent of code day 4");
+    println!("Advent of code day 5");
 
     let crates = vec![
         String::from("NDMQBPZ"),
@@ -19,7 +20,7 @@ fn main() {
     ];
 
     let file_path = get_file_path(String::from("input.txt"));
-    let result = puzzle(file_path, crates);
+    let result = puzzle_2(file_path, crates);
 
     println!("result -> {result}");
 }
@@ -79,9 +80,51 @@ fn puzzle(file_path: PathBuf, crates: Vec<String>) -> String {
     top_crates.to_string()
 }
 
+fn puzzle_2(file_path: PathBuf, crates: Vec<String>) -> String {
+    let mut top_crates = String::new();
+    let mut clone_crate = crates.clone();
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(line) = line {
+                // logic puzzle
+                let instructions: Vec<i32> = line
+                    .replace("move ", "")
+                    .replace(" from ", ",")
+                    .replace(" to ", ",")
+                    .split(",")
+                    .map(|item| item.parse::<i32>().unwrap())
+                    .collect();
+                let number_of_crates_to_move = instructions[0] as usize;
+                let move_crates_from_column = instructions[1] - 1;
+                let move_crates_to_column = instructions[2] - 1;
+                let mut take_crates_from = clone_crate
+                    .get(move_crates_from_column as usize)
+                    .unwrap()
+                    .to_string();
+                let push_crates_to = clone_crate
+                    .get(move_crates_to_column as usize)
+                    .unwrap()
+                    .to_string();
+                let pile_size = take_crates_from.len();
+                let crates_to_move: String = take_crates_from
+                    .drain(pile_size - number_of_crates_to_move..pile_size)
+                    .collect();
+                clone_crate[move_crates_from_column as usize] = take_crates_from.clone();
+                clone_crate[move_crates_to_column as usize] =
+                    push_crates_to.add(crates_to_move.as_str());
+            }
+        }
+    }
+    for pile in clone_crate.iter() {
+        let top_pile = pile.to_string().pop().unwrap();
+        top_crates.push(top_pile);
+    }
+    top_crates.to_string()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{get_file_path, puzzle};
+    use crate::{get_file_path, puzzle, puzzle_2};
 
     #[test]
     fn it_works() {
@@ -109,5 +152,33 @@ mod tests {
         let file_path = get_file_path(String::from("input.txt"));
         let result = puzzle(file_path, crates);
         assert_eq!(result, "QGTHFZBHV");
+    }
+
+    #[test]
+    fn it_works_with_puzzle_2() {
+        let crates = vec![String::from("ZN"), String::from("MCD"), String::from("P")];
+
+        let file_path = get_file_path(String::from("input.test.txt"));
+        let result = puzzle_2(file_path, crates);
+        assert_eq!(result, "MCD");
+    }
+
+    #[test]
+    fn it_works_with_whole_input_puzzle_2() {
+        let crates = vec![
+            String::from("NDMQBPZ"),
+            String::from("CLZQMDHV"),
+            String::from("QHRDVFZG"),
+            String::from("HGDFN"),
+            String::from("NFQ"),
+            String::from("DQVZFBT"),
+            String::from("QMTZDVSH"),
+            String::from("MGFPNQ"),
+            String::from("BWRM"),
+        ];
+
+        let file_path = get_file_path(String::from("input.txt"));
+        let result = puzzle_2(file_path, crates);
+        assert_eq!(result, "MGDMPSZTM");
     }
 }
