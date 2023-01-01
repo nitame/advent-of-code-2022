77d631646a027e80c9e12f90f8a293783cd0d699
nitame <>
2023-01-01 14:49:47 +0100

    day 6 puzzle 2

--- day-six/src/main.rs
+++ day-six/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("Advent of code day 6");
 
     let file_path = get_file_path(String::from("input.txt"));
-    let result = puzzle(file_path);
+    let result = puzzle_2(file_path);
 
     println!("result -> {result}");
 }
@@ -39,7 +39,6 @@ fn puzzle(file_path: PathBuf) -> i32 {
                         group_by_letter.insert(letter);
                     }
                     if packet.len() == group_by_letter.len() {
-                        println!("same length at index {}", i);
                         result = i as i32;
                         break;
                     }
@@ -50,9 +49,32 @@ fn puzzle(file_path: PathBuf) -> i32 {
     result + 4 as i32
 }
 
+fn puzzle_2(file_path: PathBuf) -> i32 {
+    let mut result = 0;
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(line) = line {
+                //puzzle logic
+                for i in 0..line.len() {
+                    let packet = &line[i..i + 14];
+                    let mut group_by_letter = HashSet::new();
+                    for letter in packet.chars() {
+                        group_by_letter.insert(letter);
+                    }
+                    if packet.len() == group_by_letter.len() {
+                        result = i as i32;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    result + 14 as i32
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{get_file_path, puzzle};
+    use crate::{get_file_path, puzzle, puzzle_2};
 
     #[test]
     fn it_works_with_input_1() {
@@ -100,5 +122,53 @@ mod tests {
 
         let result = puzzle(file_path);
         assert_eq!(result, 1658);
+    }
+
+    #[test]
+    fn it_works_with_input_1_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input-1.test.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 19);
+    }
+
+    #[test]
+    fn it_works_with_input_2_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input-2.test.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 23);
+    }
+
+    #[test]
+    fn it_works_with_input_3_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input-3.test.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 23);
+    }
+
+    #[test]
+    fn it_works_with_input_4_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input-4.test.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 29);
+    }
+
+    #[test]
+    fn it_works_with_input_5_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input-5.test.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 26);
+    }
+
+    #[test]
+    fn it_works_with_whole_input_for_puzzle_2() {
+        let file_path = get_file_path(String::from("input.txt"));
+
+        let result = puzzle_2(file_path);
+        assert_eq!(result, 2260);
     }
 }
